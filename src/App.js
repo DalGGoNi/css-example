a84a35b90d3f0714b3a2f273d3a1ba6875669963
박승찬 <tmdcks6993@naver.com>
2023-02-24 13:36:37 +0900

    파일 추가

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Button1 from './components/Button1'
+import Button2 from './components/Button2'
+import StyleComponent from './components/StyleComponent';
+import TailwindComponent from './components/TailwindComponent';
+
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <StyleComponent/>
+      <TailwindComponent />
+    </>
+  );
+}
+
+export default App;
